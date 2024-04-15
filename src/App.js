0d2c068c0df8b5c6937f7200336d6523a1f79e7c
Verda Muso <gearmobile@gmail.com>
2024-04-15 19:15:36 +0300

    add preloader component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import { Preloader } from './components/Preloader/Preloader';
 import { Book } from './components/book/Book';
 
-function App() {
-  return (
+function App({ isLoading }) {
+  return isLoading ? <Preloader /> : (
     <div className="App">
       <Book name="react" year={2023} price={12000} />
       <Book name="angular" year={2021} price={13000} />
---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,5 @@
+import './Preloader.css';
+
+export const Preloader = () => {
+    return <h3>Loading ...</h3>
+}
